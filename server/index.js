04931886c03b97b0ea775f3dbdcc28cd5cbe551a
William Nollert <will.nollert@gmail.com>
2022-03-28 09:25:11 -0500

    final cleanup and getting mongoose cluster connection up

--- server/index.js
+++ server/index.js
@@ -2,3 +2,26 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongose from "mongoose";
 import cors from "cors";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://username:Password12345@cluster0.rdekl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.port || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+//mongoose.set("useFindAndModify", false);
