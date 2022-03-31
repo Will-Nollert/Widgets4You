6f8b5fba6e2b89978481a1fffa917b6152400b17
William Nollert <will.nollert@gmail.com>
2022-03-31 17:44:18 -0500

    feat: added single product card features

--- client/src/components/Products/Products.js
+++ client/src/components/Products/Products.js
@@ -1,19 +1,28 @@
 import React from "react";
+import { Grid, CircularProgress } from "@material-ui/core";
 import { useSelector } from "react-redux";
+
 import SingleProduct from "./SingleProduct/SingleProduct";
 import useStyles from "./styles";
 
-const Products = () => {
-  const classes = useStyles();
+const Products = ({ setCurrentId }) => {
   const products = useSelector((state) => state.products);
   console.log(products);
+  const classes = useStyles();
 
   return (
-    <>
-      <div>Products</div>
-      <SingleProduct />
-      <SingleProduct />
-    </>
+    <Grid
+      className={classes.container}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {products.map((product) => (
+        <Grid key={product._id} item xs={12} sm={6} md={6}>
+          <SingleProduct product={product} setCurrentId={setCurrentId} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
--- client/src/components/Products/SingleProduct/SingleProduct.js
+++ client/src/components/Products/SingleProduct/SingleProduct.js
@@ -1,9 +1,72 @@
 import React from "react";
-import useStyles from "../styles";
-const SingleProduct = () => {
-  const classes = useStyles();
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@material-ui/core";
+import DeleteIcon from "@material-ui/icons/Delete";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import { useDispatch } from "react-redux";
+import { deleteProduct } from "../../../actions/products";
 
-  return <div>SingleProduct</div>;
+import useStyles from "./style";
+
+const SingleProduct = ({ product, setCurrentId }) => {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+
+  return (
+    <Card className={classes.card}>
+      <CardMedia
+        className={classes.media}
+        image={product.image}
+        title={product.title}
+      />
+      <div className={classes.overlay}>
+        <Typography variant="h6">{product.name}</Typography>
+      </div>
+      <div className={classes.overlay2}>
+        <Button
+          style={{ color: "black" }}
+          size="small"
+          onClick={() => setCurrentId(product._id)}
+        >
+          <MoreHorizIcon fontSize="medium" />
+        </Button>
+      </div>
+      <div className={classes.details}>
+        <Typography variant="body2" color="textSecondary">
+          {product.category.map((category) => `#${category} `)}
+        </Typography>
+      </div>
+      <Typography className={classes.title} variant="h5">
+        {product.title}
+      </Typography>
+      <CardContent>
+        <Typography
+          variant="body2"
+          color="textSecondary"
+          component="p"
+          gutterbottom="true"
+        >
+          {product.description}
+        </Typography>
+      </CardContent>
+      <CardActions className={classes.cardActions}>
+        <Button
+          size="small"
+          color="primary"
+          onClick={() => dispatch(deleteProduct(product._id))}
+        >
+          <DeleteIcon fontSize="small" />
+          Delete
+        </Button>
+      </CardActions>
+    </Card>
+  );
 };
 
 export default SingleProduct;
