fa407c14a034eef421116541e5fefa4ca5b8281c
William Nollert <will.nollert@gmail.com>
2022-04-07 11:30:16 -0500

    import syntax fix atetmpts

--- server/server.js
+++ server/server.js
@@ -7,8 +7,8 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 
-import productRoutes from "./routes/product.js";
-import userRoutes from "./routes/users.js";
+import { productRoutes } from "./routes/product.js";
+import { userRoutes } from "./routes/users.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
