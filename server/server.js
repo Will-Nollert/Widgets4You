1fa5fb45e9669c70b1755bf46cd36e24eabef335
William Nollert <will.nollert@gmail.com>
2022-04-08 10:13:14 -0500

    fix: merge attempt221

--- server/server.js
+++ server/server.js
@@ -37,7 +37,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/client/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    res.sendFile(
+      path.resolve(__dirname, "--prefic client", "build", "index.html")
+    )
   );
 } else {
   app.get("/", (req, res) => {
