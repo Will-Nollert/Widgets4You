a4b3445af02a3021e7eb8982900ec8784b7d69c2
William Nollert <will.nollert@gmail.com>
2022-04-08 09:11:13 -0500

    test

---
+++ server/server.js
@@ -0,0 +1,55 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+import productRoutes from "./routes/product.js";
+import userRoutes from "./routes/users.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+
+app.use("/products", productRoutes);
+app.use("/user", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello to This StoreFronts API");
+});
+
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+app.use(
+  bodyParser.json({ parameterLimit: 100000, limit: "50mb", extended: true })
+);
+app.use(
+  bodyParser.urlencoded({
+    parameterLimit: 100000,
+    limit: "50mb",
+    extended: true,
+  })
+);
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
