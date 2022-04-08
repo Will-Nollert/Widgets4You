354b2100010d45acaa73f0a72fa547db7035a991
William Nollert <will.nollert@gmail.com>
2022-04-08 09:57:56 -0500

    fix: merge attempt21

--- server/server.js
+++ server/server.js
@@ -48,7 +48,7 @@ app.use(
 
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use("/", express.static(path.resolve(__dirname, "./client/build")));
 // Step 2:
 app.get("*", function (request, response) {
   response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
